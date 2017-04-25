7974d4a6b54c854bca620db8df2f897d34864a76
George Higbie <gehigbie@gmail.com>
2017-04-25 00:54:17 +0000

    Configure server to listen

--- app.js
+++ app.js
@@ -14,4 +14,34 @@ const  Campground  = require("./models/campground"),
 const commentRoutes    = require("./routes/comments"),
       campgroundRoutes = require("./routes/campgrounds"),
       indexRoutes      = require("./routes/index");
-      
+
+mongoose.connect("mongodb://localhost/yelp_camp_v8");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+seedDB();
+
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Bulldogs are awesome creatures!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+app.listen(process.env.PORT, process.env.IP, () => {
+   console.log("The server is listening..."); 
+});
